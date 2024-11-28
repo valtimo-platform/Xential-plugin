44bb57d17f26a2d317ebe6040cf708ea60482503
paul-ritense <paul.van.beukering@ritense.com>
2024-11-28 13:55:05 +0100

    cleanup code

--- backend/plugin/src/main/kotlin/com/ritense/valtimo/xential/web/rest/DocumentResource.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimo/xential/web/rest/DocumentResource.kt
@@ -1,21 +1,14 @@
 package com.ritense.valtimo.xential.web.rest
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.ritense.valtimo.contract.annotation.SkipComponentScan
 import com.ritense.valtimo.contract.domain.ValtimoMediaType
 import com.ritense.valtimo.xential.domain.DocumentCreatedMessage
 import com.ritense.valtimo.xential.service.DocumentGenerationService
 import mu.KotlinLogging
-import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
 
 @RestController
 @SkipComponentScan
